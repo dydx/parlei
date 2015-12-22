80d2bdad77f267e59426364af290b349a71a4ab3
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-12-22 16:43:48 -0500

    added stuff

--- event_parser.rb
+++ event_parser.rb
@@ -12,6 +12,11 @@ events = Nsq::Consumer.new(
 loop do
   if msg = events.pop_without_blocking
     puts msg.body
+    # need to write this message to a file
+    File.open('logs/master_audit.log', 'a') do |log|
+      log.puts msg.body
+    end
+
     msg.finish
   else
     sleep 0.1
